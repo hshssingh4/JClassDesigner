cadb8e023b311d25011bafe01e0324afe16be958
Harpreet Singh <hshssingh4@gmail.com>
2016-03-31 22:17:42 -0400

    Edit Toolbar added. Implmentation still required.

--- src/jcd/gui/Workspace.java
+++ src/jcd/gui/Workspace.java
@@ -6,14 +6,24 @@
 package jcd.gui;
 
 import java.io.IOException;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import jcd.PropertyType;
+import static jcd.PropertyType.ADD_CLASS_ICON;
+import static jcd.PropertyType.ADD_CLASS_TOOLTIP;
+import static jcd.PropertyType.ADD_INTERFACE_ICON;
+import static jcd.PropertyType.ADD_INTERFACE_TOOLTIP;
+import static jcd.PropertyType.REDO_ICON;
+import static jcd.PropertyType.REDO_TOOLTIP;
+import static jcd.PropertyType.REMOVE_ICON;
+import static jcd.PropertyType.REMOVE_TOOLTIP;
+import static jcd.PropertyType.RESIZE_ICON;
+import static jcd.PropertyType.RESIZE_TOOLTIP;
 import static jcd.PropertyType.SELECTION_TOOL_ICON;
 import static jcd.PropertyType.SELECTION_TOOL_TOOLTIP;
-import jcd.controller.PageEditController;
+import static jcd.PropertyType.UNDO_ICON;
+import static jcd.PropertyType.UNDO_TOOLTIP;
 import saf.AppTemplate;
 import saf.components.AppWorkspaceComponent;
 import saf.ui.AppGUI;
@@ -35,7 +45,7 @@ public class Workspace extends AppWorkspaceComponent
     FlowPane viewToolbar;
     
     // Buttons for edit toolbar
-    Button selectionToolButton, resizeButon, addClassButton, addInterfaceButton, 
+    Button selectionToolButton, resizeButton, addClassButton, addInterfaceButton, 
             removeButton, undoButton, redoButton;
     
     // Buttons and other controls for view toolbar
@@ -63,17 +73,46 @@ public class Workspace extends AppWorkspaceComponent
     
     private void initEditToolbar()
     {
-        selectionToolButton = gui.initChildButton(editToolbar, SELECTION_TOOL_ICON.toString(), SELECTION_TOOL_TOOLTIP.toString(), false);
+        // Initialize edit toolbar
+        editToolbar = new FlowPane();
+        editToolbar.setHgap(5);
+        editToolbar.setVgap(5);
         
+        // Add buttons to edit toolbar
+        selectionToolButton = gui.initChildButton(editToolbar, SELECTION_TOOL_ICON.toString(), SELECTION_TOOL_TOOLTIP.toString(), true);
+        resizeButton        = gui.initChildButton(editToolbar, RESIZE_ICON.toString(), RESIZE_TOOLTIP.toString(), true);
+        addClassButton      = gui.initChildButton(editToolbar, ADD_CLASS_ICON.toString(), ADD_CLASS_TOOLTIP.toString(), true);
+        addInterfaceButton  = gui.initChildButton(editToolbar, ADD_INTERFACE_ICON.toString(), ADD_INTERFACE_TOOLTIP.toString(), true);
+        removeButton        = gui.initChildButton(editToolbar, REMOVE_ICON.toString(), REMOVE_TOOLTIP.toString(), true);
+        undoButton          = gui.initChildButton(editToolbar, UNDO_ICON.toString(), UNDO_TOOLTIP.toString(), true);
+        redoButton          = gui.initChildButton(editToolbar, REDO_ICON.toString(), REDO_TOOLTIP.toString(), true);
+        
+        // put it on top of the pane alongside file toolbar
+        FlowPane topPane = (FlowPane) app.getGUI().getAppPane().getTop();
+        topPane.getChildren().add(editToolbar);
     }
     
     @Override
     public void initStyle() 
     {
+        selectionToolButton.getStyleClass().add(CLASS_FILE_BUTTON);
+	resizeButton.getStyleClass().add(CLASS_FILE_BUTTON);
+	addClassButton.getStyleClass().add(CLASS_FILE_BUTTON);
+        addInterfaceButton.getStyleClass().add(CLASS_FILE_BUTTON);
+        removeButton.getStyleClass().add(CLASS_FILE_BUTTON);
+        undoButton.getStyleClass().add(CLASS_FILE_BUTTON);
+	redoButton.getStyleClass().add(CLASS_FILE_BUTTON);
     }
     
     @Override
     public void reloadWorkspace() 
     {
+        enforceLegalButtons();
+    }
+    
+    public void enforceLegalButtons()
+    {
+        for (Node b: editToolbar.getChildren())
+            ((Button)b).setDisable(false);
     }
 }
