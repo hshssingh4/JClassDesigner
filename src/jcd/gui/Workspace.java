5aa89df6f1b9648bf7d75a450299b767ae262194
Harpreet Singh <hshssingh4@gmail.com>
2016-03-31 21:11:12 -0400

    Property Type File Setup. Alongside CSS for components.

--- src/jcd/gui/Workspace.java
+++ src/jcd/gui/Workspace.java
@@ -6,6 +6,13 @@
 package jcd.gui;
 
 import java.io.IOException;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import jcd.PropertyType;
+import static jcd.PropertyType.SELECTION_TOOL_ICON;
+import static jcd.PropertyType.SELECTION_TOOL_TOOLTIP;
 import jcd.controller.PageEditController;
 import saf.AppTemplate;
 import saf.components.AppWorkspaceComponent;
@@ -19,9 +26,24 @@ public class Workspace extends AppWorkspaceComponent
 {
     // HERE'S THE APP
     AppTemplate app;
-
+    
     // IT KNOWS THE GUI IT IS PLACED INSIDE
     AppGUI gui;
+    
+    // Flow pane for edit toolbar buttons and view toolbar buttons
+    FlowPane editToolbar;
+    FlowPane viewToolbar;
+    
+    // Buttons for edit toolbar
+    Button selectionToolButton, resizeButon, addClassButton, addInterfaceButton, 
+            removeButton, undoButton, redoButton;
+    
+    // Buttons and other controls for view toolbar
+    Button zoomInButton, zoomOutButton;
+    CheckBox gridRenderCheckBox, gridSnapCheckBox;
+    
+    
+    
     
     public Workspace(AppTemplate initApp) throws IOException 
     {
@@ -30,6 +52,19 @@ public class Workspace extends AppWorkspaceComponent
 
 	// KEEP THE GUI FOR LATER
 	gui = app.getGUI();
+        
+        initEditToolbar();
+        
+        
+        
+        
+        
+    }
+    
+    private void initEditToolbar()
+    {
+        selectionToolButton = gui.initChildButton(editToolbar, SELECTION_TOOL_ICON.toString(), SELECTION_TOOL_TOOLTIP.toString(), false);
+        
     }
     
     @Override
